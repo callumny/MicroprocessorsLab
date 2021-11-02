1753a7fd121ae9283ebf2de81f28a2c9f0d7eaa5
cw619 <cw619@skbl-422-003.ph.ic.ac.uk>
2021-11-02 12:08:33 +0000

    - number of iterations now depends on PORTD input is displayed on PORTC - uses TRISC and TRISFD to define whether ports are outputs or inputs

--- main.s
+++ main.s
@@ -9,14 +9,17 @@ main:
 	org	0x100		    ; Main code starts here at address 0x100, this is to skip the access bank that stores fundamental variables (e.g. port assignment)
 start:
 	movlw 	0x0		    ; Moves 0x0 to W repositry
-	movwf	TRISC, A	    ; Port C all outputs
+	movwf	TRISC, A	    ; TRISC designates behavour to PORTC: 0x0 = 00000000 implies all pin are outputs
+	movlw   0xff		    ; Moves 0xff to W repository
+	movwf	TRISD, A	    ; TRISD designates behavour to PORTC: 0xff = 11111111 implies all pin are inputs
+	movlw 	0x0
 	bra 	test		    
 loop:
-	movff 	0x06, PORTC	    ; Move contents of address 0x06 to PORTC
+	movff 	0x06, PORTC	    ; Move contents of address 0x06 to PORTC (port reads the data)
 	incf 	0x06, W, A          ; Increment W register, 
 test:
-	movwf	0x06, A		    ; Test for end of loop condition
-	movlw 	0x63		    ;reduce number of iterations from 0x63 (big number)
+	movwf	0x06, A		    ; Upper liit of loop, e.g while i < (value stored in PORTD)
+	movf	PORTD, W, A 	    ; reduce number of iterations from 0x63 (99d)
 	cpfsgt 	0x06, A		    ; compare value stored at 0x06 to value stored in W and if value greter than that stoired in W then skip next line and exit loop
 	bra 	loop		    ; Not yet finished goto start of loop again
 	goto 	0x0		    ; Re-run program from start
