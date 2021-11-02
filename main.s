67211397f133c1ecbd835acae5f7ecffe620328c
cw619 <cw619@skbl-422-003.ph.ic.ac.uk>
2021-11-02 10:46:14 +0000

    - changing the port from B to C - changing the number of iterations

--- main.s
+++ main.s
@@ -3,20 +3,20 @@
 psect	code, abs
 	
 main:
-	org	0x0
-	goto	start
+	org	0x0		    ; org sets the assembler location counter
+	goto	start		    ; go to an address
 
-	org	0x100		    ; Main code starts here at address 0x100
+	org	0x100		    ; Main code starts here at address 0x100, this is to skip the access bank that stores fundamental variables (e.g. port assignment)
 start:
-	movlw 	0x0
-	movwf	TRISB, A	    ; Port C all outputs
-	bra 	test
+	movlw 	0x0		    ; Moves 0x0 to W repositry
+	movwf	TRISC, A	    ; Port C all outputs
+	bra 	test		    
 loop:
-	movff 	0x06, PORTB
-	incf 	0x06, W, A
+	movff 	0x06, PORTC	    ; Move contents of address 0x06 to PORTC
+	incf 	0x06, W, A          ; Increment W register, 
 test:
-	movwf	0x06, A	    ; Test for end of loop condition
-	movlw 	0x63
+	movwf	0x06, A		    ; Test for end of loop condition
+	movlw 	0x05		    ;reduce number of iterations from 0x63 (big number)
 	cpfsgt 	0x06, A
 	bra 	loop		    ; Not yet finished goto start of loop again
 	goto 	0x0		    ; Re-run program from start
