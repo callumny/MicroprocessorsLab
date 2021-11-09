88b2068ace7c5a4afbfcc52591486abd63b0b215
cw619 <cw619@skbl-422-003.ph.ic.ac.uk>
2021-11-09 10:42:33 +0000

    - write data on E - set clock and OE* with D - output the memory on external chip via leds

--- main.s
+++ main.s
@@ -8,44 +8,23 @@ main:
 
 	org	0x100		    ; Main code starts here at address 0x100, this is to skip the access bank that stores fundamental variables (e.g. port assignment)
 start:
+ 
 	movlw 	0x0		    ; Moves 0x0 to W repositry
-	movwf	TRISC, A	    ; TRISC designates behavour to PORTC: 0x0 = 00000000 implies all pin are outputs
-	movlw   0xff		    ; Moves 0xff to W repository
-	movwf	TRISD, A	    ; TRISD designates behavour to PORTC: 0xff = 11111111 implies all pin are inputs
-	movlw 	0x0
-	bra 	test		    
-loop:
-    
-	movlw	0xff                ; sets length of delay
-	movwf	0x10, A             ; move value of 5 to address 0x10
-	movlw	0xff                ; sets length of delay
-	movwf	0x11, A             ; move value of 5 to address 0x10
-	call    delayloop
+	movwf	TRISD, A	    ; TRISC designates behavour to PORTC: 0x0 = 00000000 implies all pin are outputs
+	movwf	TRISE, A	    ; TRISD designates behavour to PORTC: 0xff = 11111111 implies all pin are inputs
 	
-	
-	movff 	0x06, PORTC	    ; Move contents of address 0x06 to PORTC (port reads the data)
-	incf 	0x06, W, A          ; Increment W register, 
-		
-test:
-	movwf	0x06, A		    ; Upper liit of loop, e.g while i < (value stored in PORTD)
-	movf	PORTD, W, A 	    ; reduce number of iterations from 0x63 (99d)
-	cpfsgt 	0x06, A		    ; compare value stored at 0x06 to value stored in W and if value greter than that stoired in W then skip next line and exit loop
-	bra 	loop		    ; Not yet finished goto start of loop again
-	goto 	0x0		    ; Re-run program from start
+	movlw 	0x0		    ; move 0x0 to W repositry
+	movwf	LATD, A		    ;move 0x0 to port D
 
-		
-delayloop:
-    call smalldelay
-    decfsz 0x11
-    bra delayloop
-    return
-    
-smalldelay:
-	decfsz 0x10 ; skips next line if number in decreasing counter is 0
-	bra smalldelay ; branches back to loop
-	return
+	bsf	LATD, 0x0, 1	    ;sets the first bit in port D as a 1, to set the OE* as high, allowing writing to the external chip
+	bsf	LATD, 0x1, 1	    ;sets the 2nd bit in port D as a 1, to set clock high allowing us to change it later
 	
+	movlw	0xAA		    ;sets the w repositry at 10101010
+	movwf	LATE, A		    ;moves the w repositry to port E (what is the significance of A?)
 	
-
+	bsf	LATD, 0x1, 0	    ;sets clock low
+	bsf	LATD, 0x1, 1	    ;sets clock high (transition from low to high tells external chip to read from data pins connected to port E)
 	
+	bsf	LATD, 0x0, 0	    ; while this is low this outputs saved data from the external chip
+	goto	$		    ; this loops back to the previous command and keeps the pointer there, in theory continously outputing the signal
 end	main
