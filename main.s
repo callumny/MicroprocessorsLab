11f3d44d2a640a260b20fad9fd14d5699a413bb0
cw619 <cw619@skbl-422-003.ph.ic.ac.uk>
2021-11-09 12:14:57 +0000

    SPI code example from presentation 5_serial_parallel - used to convert serial input to parallel output and then output parallel data to LED - start: contains SPI functions - Doesnt work yet

--- main.s
+++ main.s
@@ -8,24 +8,30 @@ main:
 
 	org	0x100		    ; Main code starts here at address 0x100, this is to skip the access bank that stores fundamental variables (e.g. port assignment)
 start:
- 
-	movlw 	0x0		    ; Moves 0x0 to W repositry
- 	movwf	TRISD, A	    ; TRISC designates behavour to PORTC: 0x0 = 00000000 implies all pin are outputs
-	movwf	TRISE, A	    ; TRISD designates behavour to PORTC: 0xff = 11111111 implies all pin are inputs
+    
+        call SPI_MasterInit
+	movlw 0xAA
+	call SPI_MasterTransmit
+	goto start
+    
+SPI_MasterInit:	; Set Clock edge to negative
+	bcf	CKE2	; CKE bit in SSP2STAT, 
+	; MSSP enable; CKP=1; SPI master, clock=Fosc/64 (1MHz)
+	movlw 	(SSP2CON1_SSPEN_MASK)|(SSP2CON1_CKP_MASK)|(SSP2CON1_SSPM1_MASK)
+	movwf 	SSP2CON1, A
+	; SDO2 output; SCK2 output
+	bcf	TRISD, PORTD_SDO2_POSN, A	; SDO2 output
+	bcf	TRISD, PORTD_SCK2_POSN, A	; SCK2 output
+	return
 	
-	movlw 	0x0		    ; move 0x0 to W repositry
-	movwf	LATD, A		    ;move 0x0 to port D
-
-	bsf	LATD, 0x0, 1	    ;sets the first bit in port D as a 1, to set the OE* as high, allowing writing to the external chip
-	bsf	LATD, 0x1, 1	    ;sets the 2nd bit in port D as a 1, to set clock high allowing us to change it later
+SPI_MasterTransmit:  ; Start transmission of data (held in W)
+        movwf 	SSP2BUF, A 	; write data to output buffer
 	
-	movlw	0xAA		    ;sets the w repositry at 10101010
-	movwf	LATE, A		    ;moves the w repositry to port E (what is the significance of A?)
-	
-	bsf	LATD, 0x1, 0	    ;sets clock low
-	bsf	LATD, 0x1, 1	    ;sets clock high (transition from low to high tells external chip to read from data pins connected to port E)
-	bsf	LATD, 0x0, 0	    ; while this is low this outputs saved data from the external chip
-	goto	start		    ; this loops back to the previous command and keeps the pointer there, in theory continously outputing the signal
+Wait_Transmit:	; Wait for transmission to complete 
+        btfss 	SSP2IF		; check interrupt flag to see if data has been sent	
+	bra 	Wait_Transmit
+	bcf 	SSP2IF		; clear interrupt flag	
+	return 	
 end	main
 	
 	
