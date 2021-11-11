8b9a6e626111746a13d2f96303a6d9ff2dd2c896
cw619 <cw619@skbl-422-003.ph.ic.ac.uk>
2021-11-11 11:42:01 +0000

    SPI code example from presentation 5_serial_parallel - used to convert serial input to parallel output and then output parallel data to LED - start: contains SPI functions - now it works :)

--- main.s
+++ main.s
@@ -7,15 +7,18 @@ main:
 	goto	start		    ; go to an address
 
 	org	0x100		    ; Main code starts here at address 0x100, this is to skip the access bank that stores fundamental variables (e.g. port assignment)
+
+
 start:
     
         call SPI_MasterInit
-	movlw 0xAA
+	movlw 0x40
 	call SPI_MasterTransmit
-	goto start
+	goto $
+	
     
 SPI_MasterInit:	; Set Clock edge to negative
-	bcf	CKE2	; CKE bit in SSP2STAT, 
+	bcf	CKE2	; CKE bit in SSP2STAT,          WHAT IS SSP2STAT BIT?
 	; MSSP enable; CKP=1; SPI master, clock=Fosc/64 (1MHz)
 	movlw 	(SSP2CON1_SSPEN_MASK)|(SSP2CON1_CKP_MASK)|(SSP2CON1_SSPM1_MASK)
 	movwf 	SSP2CON1, A
@@ -32,6 +35,7 @@ Wait_Transmit:	; Wait for transmission to complete 
 	bra 	Wait_Transmit
 	bcf 	SSP2IF		; clear interrupt flag	
 	return 	
-end	main
 	
+
+end main	
 	
