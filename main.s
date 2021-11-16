0efa0174d3dbd2c4490d9880f295d8f239c0fb70
cw619 <cw619@skbl-422-003.ph.ic.ac.uk>
2021-11-16 10:12:07 +0000

    - second test of the keyboard module to see if port B and C light up with the input data from keyboard

--- main.s
+++ main.s
@@ -2,8 +2,25 @@
 
 extrn	keyboard_setup, keyboard_start  ; external subroutines
 ;extrn	LCD_Setup, LCD_Write_Message, Display_clear
+	 
+
+;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;; 
+    
+psect	code, abs	
+rst: 	org 0x0
+ 	goto	setup
+
+	; ******* Programme FLASH read Setup Code ***********************
+setup: 
+	; in the keyboard setup we select the correct bank register
+	; clears port E 
+	; makes B and C outputs 
+	call	keyboard_setup	; setup keyboard
+	goto	start
 	
-main:
-    call    keyboard_setup
-    call    keyboard_start
- end	main   
+	; ******* Main programme ****************************************
+start: 	
+	call keyboard_start
+	
+	end	rst
+ 
