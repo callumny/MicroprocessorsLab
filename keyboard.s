68f91cb152276b6cddb7584f36589d80c94ee5df
cw619 <cw619@skbl-422-003.ph.ic.ac.uk>
2021-11-18 12:11:30 +0000

    - works absolutely perfectly with debugger - assumed error coming from delay code

--- keyboard.s
+++ keyboard.s
@@ -18,24 +18,35 @@ keyboard_setup:
     bsf REPU ; bank select register - because PADCFG1 is not in access RAM
     banksel 0
     
-    ;clrf LATD, A
-    ;clrf LATC, A
-    ;clrf LATH, A
-    clrf PORTE, A
-    clrf PORTD, A
-    clrf PORTC, A
+    clrf LATD, A
+    clrf LATC, A
+    clrf LATH, A
+    ;clrf PORTE, A
+    ;clrf PORTD, A
+    ;clrf PORTC, A
     clrf LATE, A ; writes all 0's to LAT register - remembers outputs/position of pull up resistors on Port E
     
+    movlw   0x00
+    movwf   TRISD,A
+    movwf   TRISC,A
+    movwf   TRISH,A
+       
     return
 ; D should be at adress less than C, rows than columns DDDDCCCC 
     ; D at 0x06, C at 0x07
     
 keyboard_start: ; press and hold button 
     ;Finding Rows
+    
+    movlw   0x00
+    movwf   0x06,A
+    movwf   0x07,A	    ;clears the 0x06 and 0x07
+    
     movlw 0x0F; 00001111 ; PORTE 4-7 (columns) are outputs and Port E 0-3 (rows) are inputs
     movwf TRISE, A
     nop
-    ;call    lcdlp2	    ; calls a 1ms delay after setting the Tristate to allow voltage pins to settle
+    ;movlw 0x01
+    ;call  LCD_delay_ms	    ; DELAY!!!!!!
     
     movf PORTE, W, A
     movwf 0x06, A		; move data on w to Port D
@@ -47,10 +58,10 @@ keyboard_start: ; press and hold button
     movlw 0xF0			; 11110000 ; PORTE 4-7 (columns) are inputs and Port E 0-3 (rows) are outputs
     movwf TRISE, A
     
-    ;call    lcdlp2	    ;call a 1ms delay to allow voltage to stabilize
+    ;movlw 0x01
+    ;call  LCD_delay_ms    ;DELAY!!!!
     nop
     movf PORTE, W, A
-    ;call lcdlp2
     movwf 0x07, A	    ; move data on w to Port C
     
     return
@@ -61,26 +72,15 @@ Recombine:
     movf    0x06, W, A ; displyaing row nibble
     movwf   PORTD, A
     
-    movlw   0x00
-    movwf   TRISD, A	    ;set port D to outputs
-    ;call    lcdlp2	    ;calls a 1ms delay to allow voltage to stabilize
     nop
     movf    0x07, W, A ; displaying coloumn nibble
     movwf   PORTC, A	    ;moves the coloumn nibble to PORTC
-
-    movlw   0x00
-    movwf   TRISC, A	    ;set port C to outputs
-    ;call    lcdlp2	    ;calls a 1ms delay to allow voltage to stabilize
-
+    
     movf    0x06, W, A
     iorwf   0x07, 0, 0	    ;compares the contents of 0x06
     
     movwf   PORTH, A
-    movlw   0x00
-    movwf   TRISH, A
-    
-    ;movlw   0x20
-    ;call    LCD_delay_ms
+
     
     ;read the whole 8 bits
     ; and it with 0x0F for the lower 4 bits
@@ -90,6 +90,7 @@ Recombine:
 ; ** a few delay routines below here as LCD timing can be quite critical **** from LCD.s
 LCD_delay_ms:		    ; delay given in ms in W
 	movwf	LCD_cnt_ms, A
+
 lcdlp2:	movlw	250	    ; 1 ms delay
 	call	LCD_delay_x4us	
 	decfsz	LCD_cnt_ms, A
@@ -109,6 +110,11 @@ LCD_delay_x4us:		    ; delay given in chunks of 4 microsecond in W
 
 LCD_delay:			; delay routine	4 instruction loop == 250ns	    
 	movlw 	0x00		; W=0
+lcdlp1:	decf 	LCD_cnt_l, F, A	; no carry when 0x00 -> 0xff
+	subwfb 	LCD_cnt_h, F, A	; no carry when 0x00 -> 0xff
+	bc 	lcdlp1		; carry, then loop again
+	return			; carry reset so return
+
 	
     
 end    
