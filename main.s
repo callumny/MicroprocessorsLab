ab90ae7ec75a96a2e379d11a327701b5784790d3
cw619 <cw619@skbl-422-003.ph.ic.ac.uk>
2021-11-24 13:58:34 +0000

    - Trying new way of checking zero by NOTTING keybyte and using cpfsg

--- main.s
+++ main.s
@@ -1,7 +1,7 @@
 #include <xc.inc>
 
 global	start, setup
-extrn	keyboard_setup, LCD_Setup, keyboard_start, Recombine, Display_key_byte, Check_pressed, Find_index, Display_index, key_byte, Check_pressed  ; external subroutines
+extrn	keyboard_setup, LCD_Setup, keyboard_start, Recombine, Display_key_byte, Check_pressed, Find_index, Display_index, key_byte, Print  ; external subroutines
 ;extrn	LCD_Setup, LCD_Write_Message, Display_clear
 	
 ;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;; 
@@ -23,11 +23,29 @@ start:
 	call keyboard_start
 	call Recombine
 	call Display_key_byte
-	call Check_pressed
+	;call Check_pressed    ; lights go off when this is uncommented for some reason
+	
+	; NOT on keybyte
+	
+	movlw 0x00
+	cpfsgt NOT_key_byte
+	
+	'''
+	movlw 0xFF
+	cpfseq key_byte
+        return ;yes button pressed
+	bra start ;no button pressed
+	'''
+	
+	
+	;check light
+	;movlw 0x00  ; only if button is pressed
+	;movwf PORTB, A
+	
 	call Find_index
 	call Display_index
 	call Print
-	bra start ; call no_button_pressed    ; no lights on B when no key prssed
+	goto start ; call no_button_pressed    ; no lights on B when no key prssed
 
-end 
+end start
  
