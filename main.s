b3337091ade27fef867a2c05484df2bf306eba0d
cw619 <cw619@skbl-422-003.ph.ic.ac.uk>
2021-11-24 12:10:41 +0000

    -Keypad emptied

--- main.s
+++ main.s
@@ -1,7 +1,7 @@
 #include <xc.inc>
 
 global	start, setup
-extrn	keyboard_setup, keyboard_start, Recombine, Find_index,Place_index, LCD_Setup, check_light,clear_check_light, key_byte, Check_button_pressed  ; external subroutines
+extrn	keyboard_setup, LCD_Setup, keyboard_start, Recombine, Display_key_byte, Check_pressed, Find_index, Display_index, key_byte, Check_pressed  ; external subroutines
 ;extrn	LCD_Setup, LCD_Write_Message, Display_clear
 	
 ;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;; 
@@ -14,7 +14,7 @@ rst: 	org 0x0
 
 setup: 
 	call	keyboard_setup	; setup keyboard
-	;call	LCD_Setup
+	call	LCD_Setup
 	goto	start
 	
 	; ******* Main programme ****************************************
@@ -22,34 +22,12 @@ start:
 	;call clear_check_light
 	call keyboard_start
 	call Recombine
+	call Display_key_byte
+	call Check_pressed
 	call Find_index
-	call Check_button_pressed
-	
-	;clrf PORTB, A
-	movlw 0xFF
-	cpfseq key_byte
-	return;call yes_button_pressed    ; show lights on port b if key has been pressed
-	bra start; call no_button_pressed    ; no lights on B when no key prssed
-    
-	
-	
-	;movff key_byte, PORTC, A
-	call Place_index
-	;movlw 0xaa
-	;movwf 0x01, A
-	
-	;movlw	0xFF
-	;cpfsgt	key_byte        ; if keybyte is 0xFF, i.e. no button pressed, then we skip the next line and branch to the start
-	;goto	rest
-	;movlw   0xFF
-	;movwf   0x01, A        ; show 0F on portA if no button is pressed
-	;bra	start
-	
-rest:
-	;call check_light
-	call Find_index
-	call Place_index
-	goto start
-		
+	call Display_index
+	call Print
+	bra start ; call no_button_pressed    ; no lights on B when no key prssed
+
 end 
  
