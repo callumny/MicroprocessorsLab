f13852b87a9d4dbe35e01c072548b66aa099ff03
cw619 <cw619@skbl-422-003.ph.ic.ac.uk>
2021-11-24 11:45:52 +0000

    - Neil has worked on this to no avail, will write fresh code for keypad - cant test if button is pressed - cant get the correct index

--- main.s
+++ main.s
@@ -1,7 +1,7 @@
 #include <xc.inc>
 
 global	start, setup
-extrn	keyboard_setup, keyboard_start, Recombine, Find_index,Place_index, LCD_Setup, check_light,clear_check_light, key_byte  ; external subroutines
+extrn	keyboard_setup, keyboard_start, Recombine, Find_index,Place_index, LCD_Setup, check_light,clear_check_light, key_byte, Check_button_pressed  ; external subroutines
 ;extrn	LCD_Setup, LCD_Write_Message, Display_clear
 	
 ;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;; 
@@ -22,11 +22,28 @@ start:
 	;call clear_check_light
 	call keyboard_start
 	call Recombine
+	call Find_index
+	call Check_button_pressed
 	
-	movlw	0xFF
-	cpfsgt	key_byte
-	goto	rest
-	bra	start
+	;clrf PORTB, A
+	movlw 0xFF
+	cpfseq key_byte
+	return;call yes_button_pressed    ; show lights on port b if key has been pressed
+	bra start; call no_button_pressed    ; no lights on B when no key prssed
+    
+	
+	
+	;movff key_byte, PORTC, A
+	call Place_index
+	;movlw 0xaa
+	;movwf 0x01, A
+	
+	;movlw	0xFF
+	;cpfsgt	key_byte        ; if keybyte is 0xFF, i.e. no button pressed, then we skip the next line and branch to the start
+	;goto	rest
+	;movlw   0xFF
+	;movwf   0x01, A        ; show 0F on portA if no button is pressed
+	;bra	start
 	
 rest:
 	;call check_light
