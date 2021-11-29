349e2952bcb538225f937734ed50398d399e09c2
cw619 <cw619@skbl-422-003.ph.ic.ac.uk>
2021-11-29 12:30:34 +0000

    two_keypad implementing button presss fo e and d check

--- main_Two_Keypads.s
+++ main_Two_Keypads.s
@@ -1,7 +1,7 @@
 #include <xc.inc>
 
 global	start, setup
-extrn	keyboard_setup_E, LCD_Setup, keyboard_start_E, Recombine_E, Split_NOT_key_byte_E, Display_key_byte_E, Display_NOT_key_byte_E, Check_pressed_E, Find_index_E, Display_index_E, key_byte_E, NOT_key_byte_E, NOT_key_byte_low_E, NOT_key_byte_high_E, index_E, zero_byte, invalid_index  ; external subroutines
+extrn	Display_E_and_D_press_state, E_and_D_press_state, Is_button_D_pressed, Check_pressed_2_D, Is_button_E_pressed, Check_pressed_2_E, keyboard_setup_E, LCD_Setup, keyboard_start_E, Recombine_E, Split_NOT_key_byte_E, Display_key_byte_E, Display_NOT_key_byte_E, Check_pressed_E, Find_index_E, Display_index_E, key_byte_E, NOT_key_byte_E, NOT_key_byte_low_E, NOT_key_byte_high_E, index_E, zero_byte, invalid_index  ; external subroutines
 ;extrn	LCD_Setup, LCD_Write_Message, Display_clear
 	
 ;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;; 
@@ -31,9 +31,9 @@ start:
 	call Recombine_D
 	
 	;check if button is pressed
-	call Check_pressed_E   ; returns 0x00 in Wreg if no buton is pressed
-	cpfslt zero_byte, A
-	bra start              ; if no button is pressed
+	;call Check_pressed_E   ; returns 0x00 in Wreg if no buton is pressed
+	;cpfslt zero_byte, A
+	;bra start              ; if no button is pressed
 	
 	
 	;;call Recombine_D
@@ -49,15 +49,22 @@ start:
 	cpfslt zero_byte, A
 	call Check_pressed_D          ; if no button on E is pressed
 	; if button on E is pressed
+	call Is_button_E_pressed
+	call Is_button_D_pressed
+	call E_and_D_press_state
+	call Display_E_and_D_press_state
 	
-	   
-	   
+	
+	;movf button_on_E_pressed
+	;cpfslt zero_byte, A
+	;call Check_pressed_D 
+	
 	   ; continue given button is pressed
-	call Display_key_byte_E
+	;call Display_key_byte_E
 	;call Display_key_byte_D
 	
-	call Find_index_E
-	call Display_index_E
+	;call Find_index_E
+	;call Display_index_E
 	goto start ; call no_button_pressed    ; no lights on B when no key prssed
 
 end start
