5434e795a97c8c45295d36122fcb309f3fada886
cw619 <cw619@skbl-422-003.ph.ic.ac.uk>
2021-11-29 12:05:54 +0000

    main for single keyboard - byte reserved in memory to give status if button is pressed

--- main.s
+++ main.s
@@ -1,7 +1,7 @@
 #include <xc.inc>
 
 global	start, setup
-extrn	keyboard_setup, LCD_Setup, keyboard_start, Recombine, Split_NOT_key_byte, Display_key_byte, Display_NOT_key_byte, Check_pressed, Find_index, Display_index, key_byte, NOT_key_byte, NOT_key_byte_low, NOT_key_byte_high, Print, index, zero_byte, invalid_index  ; external subroutines
+extrn	Is_button_pressed, button_pressed, keyboard_setup, LCD_Setup, keyboard_start, Recombine, Split_NOT_key_byte, Display_key_byte, Display_NOT_key_byte, Check_pressed, Find_index, Display_index, key_byte, NOT_key_byte, NOT_key_byte_low, NOT_key_byte_high, Print, index, zero_byte, invalid_index  ; external subroutines
 ;extrn	LCD_Setup, LCD_Write_Message, Display_clear
 	
 ;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;; 
@@ -28,16 +28,26 @@ start:
 	movwf PORTH, A	;
 	movwf PORTC, A
 	call Recombine
-	;check if button is pressed
-	call Check_pressed   ; returns 0x00 in Wreg if no buton is pressed
+	
+	;call Is_button_pressed
+	movf button_pressed
+	movwf PORTD
 	cpfslt zero_byte, A
 	bra start            ; if no button is pressed
+	
+	
+	;movf button_pressed
+	;movwf PORTH
+	;check if button is pressed
+	;call Check_pressed   ; returns 0x00 in Wreg if no buton is pressed
+	;cpfslt zero_byte, A
+	;bra start            ; if no button is pressed
 	
 	; continue given button is pressed
 	call Display_key_byte
 	call Find_index
 	call Display_index
-	call Print
+	;call Print
 	goto start ; call no_button_pressed    ; no lights on B when no key prssed
 
     
