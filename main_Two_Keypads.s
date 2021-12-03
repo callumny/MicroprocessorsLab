83d78cf6097248186043ae7da653cace18260ea0
cw619 <cw619@skbl-422-003.ph.ic.ac.uk>
2021-12-03 10:03:06 +0000

    Working two keypad - tidied up slightly - checks at least one button is pressed - checks if button press is valid -- invalid button presses are when more than one button is pressed, so if there are two buttons pressed on one key pad or two buttons pressed in total (like one on each keypad) - index so A:1, B:2, ..., Z:26, SF6:32. - index for invalid keypress when more than one button pressed on a keypad is 0xFF - A-P on keypad1 connected to portE, Q-SF6 on keypad2 connected to portD - commenting and structuring needs tidying

--- main_Two_Keypads.s
+++ main_Two_Keypads.s
@@ -7,13 +7,13 @@ global	start, setup
 extrn    Two_keypad_setup, button_pressed_state, \
     Display_E_and_D_press_state, E_and_D_press_state, Is_button_D_pressed, Check_pressed_2_D, Is_button_E_pressed, Check_pressed_2_E, \
     Keypad_start_E, Keypad_start_D, \
-    Recombine_E, Split_NOT_key_byte_E, Display_key_byte_E, Display_NOT_key_byte_E, Check_pressed_E,\
+    Recombine_E, Split_NOT_key_byte_E, Display_key_byte_E, Display_NOT_key_byte_E,\
     Find_index_E, Display_index_E, key_byte_E, NOT_key_byte_E, NOT_key_byte_low_E, NOT_key_byte_high_E, index_E,\
-    Recombine_D, Split_NOT_key_byte_D, Display_key_byte_D, Display_NOT_key_byte_D, Check_pressed_D,\
+    Recombine_D, Split_NOT_key_byte_D, Display_key_byte_D, Display_NOT_key_byte_D,\
     Find_index_D, Display_index_D, key_byte_D, NOT_key_byte_D, NOT_key_byte_low_D, NOT_key_byte_high_D, index_D,\
     Display_E_press_state, Display_D_press_state,\
     button_pressed_state,\
-    Invalid_button_press,\
+    Invalid_button_press_one,\
     Display_two_keypad_index,\
     zero_byte, FF_byte, invalid_index,\
     index, Two_keypad_find_index,\
@@ -43,83 +43,53 @@ start:
 	movwf PORTH, A	;
 	movwf PORTJ, A	;
 	movwf PORTB, A	;	
-	;movwf PORTC, A
-	;;call two_keypad_start   ; identifies which pad is pressed and 
 	
 	call Keypad_start_E
 	call Recombine_E
 	call Keypad_start_D
 	call Recombine_D
 	
-	; if button on E is pressed
 	call Is_button_E_pressed
 	call Is_button_D_pressed
 	;call Display_E_press_state
 	;call Display_D_press_state
 	call E_and_D_press_state
-	call Display_E_and_D_press_state
-	;call Display_E_and_D_press_state
-	
-	; call find_index_e or d doesnt work here before he button_pressed_state comparison
-        call Find_index_E
-	call Find_index_D
+	call Display_E_and_D_press_state ; F0 for portD, 0F for port E, FF for both pressed, 00 for neither pressed
+
+        call Find_index_E   ; as for one keypad
+	call Find_index_D   ; as for one keypad 
 	
 	
 	movf button_pressed_state, 0, 0 ; 0x00 for no button pressed,0x0F for E pressed only, 0xF0 for D pressed only, 0xFF for E and D button is pressed 
 	cpfslt zero_byte, A    ;   skip if f less than w, i.e skip if zero byte is less than button_pressed , which occurs whenever a button is pressed
-	bra start     
+	bra start    
 	
-        ;call Find_index_E
-	;call Find_index_D
-	
-	;check the buttons pressed are on only one port
+	;check the buttons pressed are on only one port, i.e. check buttons on both ports keypads havent been pressed simultaneously
 	movf button_pressed_state, 0, 0 ; 0x00 for no button pressed,0x0F for E pressed only, 0xF0 for D pressed only, 0xFF for E and D button is pressed       ; 
 	cpfsgt FF_byte, A    ;   skip if f greater than w, i.e skip if FF byte is greater than button_pressed , which occurs whenever only one button on D or E is pressed, i.e valid press
-	call Invalid_button_press ;bra start  ;invalid
+	call Invalid_button_press_one ; shows 0x11, error light for if two buttons have been pressed, one on each keypad
 	
-	movf button_pressed_state, 0, 0
-	cpfsgt FF_byte, A      ; for some reason isnt going to next line when button_pressed_state is FF, although it does for the above test
+	movf button_pressed_state, 0, 0 ; same check as above but now branches to start
+	cpfsgt FF_byte, A
 	bra start
-;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
-	;checks that only one button pressed on a given port
-        ;movf index_E, 0, 0
-	;cpfsgt invalid_index, A            ;   skip if f greater than w, i.e skip if FF byte is greater than button_pressed , which occurs whenever only on button on D or E is pressed, i.e valid press
-	;call Invalid_button_press_on_port_E
-	
-	;movf index_E, 0, 0
-	;cpfsgt invalid_index, A
-	;bra start
-	
-        ;movf index_D, 0, 0
-	;cpfsgt invalid_index, A            ;   skip if f greater than w, i.e skip if FF byte is greater than button_pressed , which occurs whenever only on button on D or E is pressed, i.e valid press
-	;call Invalid_button_press_on_port_D
-	
-	;movf index_D, 0, 0
-	;cpfsgt invalid_index, A
-	;bra start
-;;;;;;;;;;;;;;;;;;;
-	
-	;better version of check for if the press is an invalid press on just one ports keypad
+
+	; retrieves index by identifying whether keypad E or D is pressed and then using single keypad indices
 	call Two_keypad_find_index
 	
+	;check only one button is pressed on a given keypad, e.g if two buttons are pressed on keypad E then show error
 	movf index, 0, 0
 	cpfsgt invalid_index, A            ;   skip if f greater than w, i.e skip if FF byte is greater than button_pressed , which occurs whenever only on button on D or E is pressed, i.e valid press
-	call Invalid_button_press_two         ; calls same invalid button pres function as if it was for two buttons pressed with one button on each keypad 
-	
+	call Invalid_button_press_two      ; shows 0x22, error light for if two buttons have been pressed on one keypad
 	movf index, 0, 0
 	cpfsgt invalid_index, A
 	bra start
 	
-        movlw 0xFF
-	movwf PORTJ, A
-
+	; lights to show if checks have been passed
+        ;movlw 0xFF
+	;movwf PORTJ, A
 	
-	;call Display_key_byte_D
 	call Display_two_keypad_index   ; displays index on portH
-	;movlw 3
-	;movwf PORTB, A
 
-	;check if e or d is chosen
 	movlw 100000000000
 	call LCD_delay_ms; external subroutines
 	nop
