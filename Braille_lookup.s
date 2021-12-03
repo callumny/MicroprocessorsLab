6a6dccdcb157875953c60359be344bb57db245bd
cw619 <cw619@skbl-422-003.ph.ic.ac.uk>
2021-12-03 14:09:23 +0000

    braille look up dosnt work

--- Braille_lookup.s
+++ Braille_lookup.s
@@ -5,7 +5,7 @@ global  Braille_table_1
 psect	code, abs
 Braille_table_1:
     addwf PCL
-    retlw 00000000B;  shouldn't be an index thta corresponds to this, just a space to make the indices correct
+    retlw 00000000B;  shouldn't be an index that corresponds to this, just a space to make the indices correct
     retlw 01000000B;    A
     retlw 01100000B;    B
     retlw 01000100B;	C
@@ -16,11 +16,12 @@ Braille_table_1:
     retlw 01100010B;	H
     retlw 00100100B;	I
     retlw 00100110B;	J
-    ;retlw 01010000B;	K
-    ;retlw 01110000B;	L
-    ;retlw 01010100B;	M
-    ;retlw 01010110B;	N
-    ;retlw 01010010B;	O
-    ;retlw 01110100B;  P
-   
+    retlw 01010000B;	K
+    retlw 01110000B;	L
+    retlw 01010100B;	M
+    retlw 01010110B;	N
+    retlw 01010010B;	O
+    retlw 01110100B;    P
+
+
 
