dc282deb170f35c7feea0cbb727a2a0ee7e32124
cw619 <cw619@skbl-422-003.ph.ic.ac.uk>
2021-12-03 12:01:05 +0000

    braille_lookup, doesnt do anything yet

--- Braille_lookup.s
+++ Braille_lookup.s
@@ -1,13 +1,13 @@
 #include <xc.inc>
 
-global  Braille_table
+global  Braille_table_1
 
 psect	code, abs
-Braille_table:
+Braille_table_1:
     addwf PCL
     retlw 00000000B;  shouldn't be an index thta corresponds to this, just a space to make the indices correct
-    retlw 01000000B;  A
-    retlw 01100000B;  B
+    retlw 01000000B;    A
+    retlw 01100000B;    B
     retlw 01000100B;	C
     retlw 01000110B;	D
     retlw 01000010B;	E
@@ -16,11 +16,11 @@ Braille_table:
     retlw 01100010B;	H
     retlw 00100100B;	I
     retlw 00100110B;	J
-    retlw 01010000B;	K
-    retlw 01110000B;	L
-    retlw 01010100B;	M
-    retlw 01010110B;	N
-    retlw 01010010B;	O
-    retlw 01110100B;  P
-    end
+    ;retlw 01010000B;	K
+    ;retlw 01110000B;	L
+    ;retlw 01010100B;	M
+    ;retlw 01010110B;	N
+    ;retlw 01010010B;	O
+    ;retlw 01110100B;  P
+   
 
