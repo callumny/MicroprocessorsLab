682f0705ce69ec782af65bc3a81df6a9dc19df54
cw619 <cw619@skbl-422-003.ph.ic.ac.uk>
2021-12-07 12:11:01 +0000

    main_two_keypads - checks enter, checks length - enters new display sub routine if enter or length is greater than 16 - so can read indices back in in this subroutine and use decfz index_counter to read the correct number of indices back in

--- main_Two_Keypads.s
+++ main_Two_Keypads.s
@@ -1,6 +1,6 @@
 #include <xc.inc>
 
-global	start, setup
+global	start, setup, index_counter
 ;extrn  Two_keypad_setup, button_pressed_state, Display_E_and_D_press_state, E_and_D_press_state, Is_button_D_pressed, Check_pressed_2_D, Is_button_E_pressed, button_pressed_E,button_pressed_D, Check_pressed_2_E, Keypad_start_E, Keypad_start_D, Recombine_E, Recombine_D, Split_NOT_key_byte_E, Display_key_byte_E, Display_NOT_key_byte_E, Check_pressed_E, Find_index_E, Display_index_E, key_byte_E, NOT_key_byte_E, NOT_key_byte_low_E, NOT_key_byte_high_E, index_E, zero_byte, invalid_index  ; external subroutines
 
     
@@ -20,7 +20,11 @@ extrn    Two_keypad_setup, button_pressed_state, \
     Invalid_button_press_on_port_D,\
     Invalid_button_press_on_port_E,\
     Invalid_button_press_two,\
-    LCD_delay_ms	; external subroutines
+    LCD_delay_ms,\
+    Display_index_counter,\
+    Check_enter, Check_length,\
+    Enter_state, Length_state,\
+    Display_index_counter_word; external subroutines
 ; external subroutines	LCD_Setup, LCD_Write_Message, Display_clear
 	
 ;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;; 
@@ -63,7 +67,7 @@ start:
 	;call Display_E_press_state
 	;call Display_D_press_state
 	call E_and_D_press_state
-	call Display_E_and_D_press_state ; F0 for portD, 0F for port E, FF for both pressed, 00 for neither pressed
+	;call Display_E_and_D_press_state ; F0 for portD, 0F for port E, FF for both pressed, 00 for neither pressed
 
         call Find_index_E   ; as for one keypad
 	call Find_index_D   ; as for one keypad 
@@ -97,20 +101,26 @@ start:
         ;movlw 0xFF
 	;movwf PORTJ, A
 	
-	;call Display_two_keypad_index   ; displays index on portH
+	call Display_two_keypad_index   ; displays index on portH
 
 	;;;;;; we have index !!!!
 	;increment index_counter
-	;movlw 1
-	;addwf index_counter, A
+	movlw 1
+	addwf index_counter, A
 	
+	call Check_enter ; writes the state to enter_length_check_byte
+	call Check_length
 	
-	;call Check_enter ; writes the state to enter_length_check_byte
-	;compare enter_length_check_byte and skip line, only chekc i f length is greater than 16 if enter is not pressed
-;call Check_length ; writes the state tpo enter_length_check_byte
-	; compare enter_length_check_byte and either excute disply routine or continue reading in indcies from keypresses 
-	;DISPLAY ROUNTINE IF CHECK SUCCESS
+	movf Enter_state, 0, 0 ; same check as above but now branches to start
+	cpfsgt FF_byte, A ; no enter pressed
+	bra Display       ;enter pressed
+	;continue to check length
+
+	movf Length_state, 0, 0 ; 0x00 for no button pressed,0x0F for E pressed only, 0xF0 for D pressed only, 0xFF for E and D button is pressed 
+	cpfsgt FF_byte, A    ;  skip if f less than w, i.e skip if zero byte is less than button_pressed , which occurs whenever a button is pressed  
+	bra Display;display subroutine ;length i gtretaer than 16
 	
+	call	Display_index_counter 
 	
 	; OUR INDEX IS NOT AN ENTER AND OUR WORD IS NOT TOO LONG
 	; call save_current_index
@@ -121,13 +131,23 @@ start:
 	rlncf index, 0, 0  ; moves 2 x index to W, no carr bit has two most significant bits are zero anyways
         call Braille_table
 	movwf PORTB, A
-	goto start ; call no_button_pressed    ; no lights on B when no key prssed
-	
-	movlw 100000000000
+	movlw 10000000       ; LCD delay ms has a limit!!!!!!!!!!!!!
+	call LCD_delay_ms; external subroutines
+	call LCD_delay_ms; external subroutines
 	call LCD_delay_ms; external subroutines
 	nop
+	movlw 0x00
+	movwf PORTB, A
 	goto start
-	
+Display:
+    ; needs to read indexes in turn and display them
+    call Display_index_counter_word ; should only show 16
+    movlw 10000000       ; LCD delay ms has a limit!!!!!!!!!!!!!
+    call LCD_delay_ms; external subroutines
+    call LCD_delay_ms; external subroutines
+    call LCD_delay_ms; external subroutines
+    bra setup	
+    
 Braille_table:
     addwf PCL, A
     retlw 00000000B;  shouldn't be an index thta corresponds to this, just a space to make the indices correct
