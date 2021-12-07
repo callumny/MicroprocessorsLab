21e6863b7914fe96ab006dc8084d2e9a5a6d611d
cw619 <cw619@skbl-422-003.ph.ic.ac.uk>
2021-12-07 16:23:35 +0000

    save_index_fsr
    currently preventing main from running for some reason

--- save_index_fsr.s
+++ save_index_fsr.s
@@ -1,23 +1,33 @@
+#include <xc.inc>
+    
+global        Save_current_index, Read_each_index, Set_saving_lfsr ;also letters 1-17    
 extrn    index
-global        index_save, enter_state, save_current_index, read_each_index ;also letters 1-17    
+    
     
 psect	udata_bank4 ; reserve data anywhere in RAM (here at 0x400)
 letter_array:    ds 16 ; reserve 128 bytes for message data
 
 psect code, abs
 
-lfsr 0, letter_array ; loads letter_array to fsr, so we can point at data adreses more effectively
-
-save_current_index:
-    movff index, POSTINC0;save_the index in correct position in letter array, this will also increment the fsr
+Set_saving_lfsr:
+    ;bcf	STATUS, 0	; point to Flash program memory 
+    ;bcf	CFGS	; point to Flash program memory  
+    ;bsf	EEPGD 	; access Flash program memory
+    lfsr 0, letter_array ; loads letter_array to fsr, so we can point at data adreses more effectively
     return
     
-read_each_index:
+Save_current_index:
+    ;movff index, INDF0 
+    movff index, POSTINC0 ;save_the index in correct position in letter array, this will also increment the fsr
+    ;movff INDF0, 0x06
+    return
+    
+Read_each_index:
     lfsr 1, letter_array
-    movff POSTINC1, index_read
+    movf POSTINC1, 0, 0     ; moves current index being read into w register
     return
     
-initialise_letter_array:
-    
+Initialise_letter_array:
+    return
     
 
